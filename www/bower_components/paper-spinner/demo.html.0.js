f8e71d5f5659cb8a3ef76b282bb24c793885982e
JoshuaDraxten <josh4jc.exe@gmail.com>
2015-01-07 15:32:20 -0800

    Messed everything up with polymer
    Just committing so I can revert

--- www/bower_components/paper-spinner/demo.html.0.js
+++
@@ -1,9 +0,0 @@
-
-    var t = document.querySelector('template');
-    t.toggle = function() {
-      var spinners = document.querySelectorAll('paper-spinner');
-      Array.prototype.forEach.call(spinners, function(spinner) {
-        spinner.active = !spinner.active;
-      });
-    }
-  
